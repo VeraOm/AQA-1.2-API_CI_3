df7202e0266e767be7c7f56b0dca05f3d79b2cd0
VeraOm <yakov_yooy@aol.com>
2020-05-29 14:16:24 +0600

    Исправлена ошибка в тесте Задачи 3

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -15,7 +15,7 @@ public class PostmanEchoTest {
                 .post("/post")
         .then()
                 .statusCode(200)
-                .body("data.toString()=='Some data'", is(true))
+                .body("data.toString()=='some data'", is(true))
         ;
 
     }
